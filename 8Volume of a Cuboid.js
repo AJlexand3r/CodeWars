e42d97ac6d4e41816c5a564467db8009c96fbccd
AJlexand3r <Aliengoodgood@gmail.com>
2023-06-18 01:26:33 +0300

    8

---
+++ 8Volume of a Cuboid.js
@@ -0,0 +1,8 @@
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      let volume = length*width*height
+      console.log(volume)
+      return volume
+    }
+  }
+  Kata.getVolumeOfCuboid(2,3,4)
